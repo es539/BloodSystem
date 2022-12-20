29907c0b5e08cfa58c9c61ac93e928b1add66a65
Esraa Aboshady <esso.aboshady2001@gmail.com>
2022-12-21 00:39:02 +0200

    back

--- back/demo/src/main/java/com/example/demo/Registration/user.java
+++ back/demo/src/main/java/com/example/demo/Registration/user.java
@@ -1,5 +1,89 @@
 package com.example.demo.Registration;
 
 public class user {
+    long id;
+    String password;
+    String name;
+    int age;
+    float weight;
+    String bloodtype;
+    String address;
+    String city;
+    String region;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public String getBloodtype() {
+        return bloodtype;
+    }
+
+    public void setBloodtype(String bloodtype) {
+        this.bloodtype = bloodtype;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+    public boolean canDonate(String BT){
+        //compare betn BT and user bloodtype
+        return true;
+    }
 }
