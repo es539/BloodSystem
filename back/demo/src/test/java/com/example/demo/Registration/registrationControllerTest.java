884b61847d75719ed96c24eab306974b5f377a5e
Esraa Aboshady <esso.aboshady2001@gmail.com>
2022-12-21 01:08:56 +0200

    test

--- back/demo/src/test/java/com/example/demo/Registration/registrationControllerTest.java
+++ back/demo/src/test/java/com/example/demo/Registration/registrationControllerTest.java
@@ -1,8 +1,54 @@
 package com.example.demo.Registration;
 
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class registrationControllerTest {
 
+    //user
+    @Test
+    void givenRightidandRightpasswordThenTrue() {
+        registrationController test = new registrationController();
+        assertTrue(test.validateUserInfo(110756,"shoee45"));
+    }
+    @Test
+    void givenRightidandWrongpasswordThenTrue() {
+        registrationController test = new registrationController();
+        assertFalse(test.validateUserInfo(110756,"go"));
+    }
+    @Test
+    void givenWrongidandRightpasswordThenTrue() {
+        registrationController test = new registrationController();
+        assertFalse(test.validateUserInfo(1110756,"shoee45"));
+    }
+    @Test
+    void givenWrongidandWrongpasswordThenTrue() {
+        registrationController test = new registrationController();
+        assertFalse(test.validateUserInfo(1110756,"go"));
+    }
 
+    //authority
+    @Test
+    void givenRightemailandRightpasswordThenTrue() {
+        registrationController test = new registrationController();
+        assertTrue(test.validateAuthorityInfo("dar.dar.dar@gmail.com","123456789"));
+    }
+    @Test
+    void givenRightemailandWrongpasswordThenTrue() {
+        registrationController test = new registrationController();
+        assertFalse(test.validateAuthorityInfo("gg@gmail.com","go"));
+    }
+    @Test
+    void givenWrongemailandRightpasswordThenTrue() {
+        registrationController test = new registrationController();
+        assertFalse(test.validateAuthorityInfo("hh@gmail.com","d5d"));
+    }
+    @Test
+    void givenWrongemailandWrongpasswordThenTrue() {
+        registrationController test = new registrationController();
+        assertFalse(test.validateAuthorityInfo("hh@gmail.com","go"));
+    }
 }
