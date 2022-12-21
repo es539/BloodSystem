a1ffcfe042d8f1838cce7e3ce8af56229760e4c4
Salma Saeed <101990421+SalmaSaeed29@users.noreply.github.com>
2022-12-21 02:17:29 +0200

    back

--- back/demo/src/main/java/com/example/demo/Registration/registration.java
+++ back/demo/src/main/java/com/example/demo/Registration/registration.java
@@ -1,6 +1,7 @@
 package com.example.demo.Registration;
 
 import com.example.demo.DataBase.DB;
+import com.example.demo.Registration.*;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -8,44 +9,10 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/savior")
 
 public class registration {
-    public static authority authData = new authority();
-    @GetMapping("/signUpA")
 
-    public String signUp(@RequestParam String email, @RequestParam String phone, @RequestParam String pass, @RequestParam String name, @RequestParam String adrs,
-                         @RequestParam String region, @RequestParam String tax, @RequestParam String start, @RequestParam String end,
-                         @RequestParam String donationF, @RequestParam String donationT,
-                         @RequestParam int EAplus, @RequestParam int NAplus, @RequestParam int EAminus, @RequestParam int NAminus,
-                         @RequestParam int EBplus, @RequestParam int NBplus, @RequestParam int EBminus, @RequestParam int NBminus,
-                         @RequestParam int EABplus, @RequestParam int NABplus, @RequestParam int EABminus, @RequestParam int NABminus,
-                         @RequestParam int EOplus, @RequestParam int NOplus, @RequestParam int EOminus, @RequestParam int NOminus){
-        DB adding = new DB();
-        authData.setEmail(email); authData.setPhone(phone); authData.setPassword(pass); authData.setName(name);
-        authData.setAddress(adrs); authData.setRegion(region); authData.setTax(tax);
-        authData.setStartWork(start); authData.setEndWork(end);authData.setDonationtimeFrom(donationF);authData.setDonationtimeTo(donationT);
-        authData.setE_Aplus(EAplus); authData.setN_Aplus(NAplus); authData.setE_Aminus(EAminus); authData.setN_Aminus(NAminus);
-        authData.setE_Bplus(EBplus); authData.setN_Bplus(NBplus); authData.setE_Bminus(EBminus); authData.setN_Bminus(NBminus);
-        authData.setE_ABplus(EABplus); authData.setN_ABplus(NABplus); authData.setE_ABminus(EABminus); authData.setN_ABminus(NABminus);
-        authData.setE_Oplus(EOplus); authData.setN_Oplus(NOplus); authData.setE_Ominus(EOminus); authData.setN_Ominus(NOminus);
-        String response;
-        if(adding.checkForNoduplicateAuthorities(tax)==false){
-            response = adding.addAuthority(authData);
-        }else{
-            response = "invalid";
-        }
-        return response;
-    }
-
-    @GetMapping("/signInA")
-    public String signIn(@RequestParam String tax, @RequestParam String password){
-        registrationController x = new registrationController();
-        boolean valid = x.validateAuthorityInfo(tax, password);
-        if (valid){
-            DB profile = new DB();
-            authData = profile.getAuthData(tax, password);
-            return "True";
-        }
-        return "False";
-    }
+//    enum Response {
+//        DONE, ERROR, NOT_FOUND
+//    }
 
     public static user userData = new user();
     @GetMapping("/signInI")
@@ -77,6 +44,71 @@ public class registration {
         String response;
         if(adding.checkForNoduplicateUsers(id)==false){
             response = adding.addUser(userData);
+        }else{
+            response = "invalid";
+        }
+        return response;
+    }
+
+    @GetMapping("/gUn")
+    public String gUserName(){
+        return userData.getName();
+    }
+    @GetMapping("/gUi")
+    public String gUserId(){ return  String.valueOf(userData.getId()); }
+    @GetMapping("/gUa")
+    public String gUserAddress(){
+        return userData.getAddress();
+    }
+    @GetMapping("/gUr")
+    public String gUserRegion(){ return userData.getRegion(); }
+
+    @GetMapping("/gUc")
+    public String gUserCity(){
+        DB db = new DB();
+        return db.getCity(userData.getRegion()); }
+
+    @GetMapping("/gUw")
+    public String gUserWeight(){ return String.valueOf(userData.getWeight()); }
+    @GetMapping("/gUg")
+    public String gUserAge(){ return String.valueOf(userData.getAge()); }
+    @GetMapping("/gUt")
+    public String gUserType(){
+        return userData.getBloodtype();
+    }
+
+    public static authority authData = new authority();
+    @GetMapping("/signInA")
+    public String signIn(@RequestParam String tax, @RequestParam String password){
+        registrationController x = new registrationController();
+        boolean valid = x.validateAuthorityInfo(tax, password);
+        if (valid){
+            DB profile = new DB();
+            authData = profile.getAuthData(tax, password);
+            return "True";
+        }
+        return "False";
+    }
+
+    @GetMapping("/signUpA")
+    public String signUp(@RequestParam String email, @RequestParam String phone, @RequestParam String pass, @RequestParam String name, @RequestParam String adrs,
+                         @RequestParam String region, @RequestParam String tax, @RequestParam String start, @RequestParam String end,
+                         @RequestParam String donationF, @RequestParam String donationT,
+                         @RequestParam int EAplus, @RequestParam int NAplus, @RequestParam int EAminus, @RequestParam int NAminus,
+                         @RequestParam int EBplus, @RequestParam int NBplus, @RequestParam int EBminus, @RequestParam int NBminus,
+                         @RequestParam int EABplus, @RequestParam int NABplus, @RequestParam int EABminus, @RequestParam int NABminus,
+                         @RequestParam int EOplus, @RequestParam int NOplus, @RequestParam int EOminus, @RequestParam int NOminus){
+        DB adding = new DB();
+        authData.setEmail(email); authData.setPhone(phone); authData.setPassword(pass); authData.setName(name);
+        authData.setAddress(adrs); authData.setRegion(region); authData.setTax(tax);
+        authData.setStartWork(start); authData.setEndWork(end);authData.setDonationtimeFrom(donationF);authData.setDonationtimeTo(donationT);
+        authData.setE_Aplus(EAplus); authData.setN_Aplus(NAplus); authData.setE_Aminus(EAminus); authData.setN_Aminus(NAminus);
+        authData.setE_Bplus(EBplus); authData.setN_Bplus(NBplus); authData.setE_Bminus(EBminus); authData.setN_Bminus(NBminus);
+        authData.setE_ABplus(EABplus); authData.setN_ABplus(NABplus); authData.setE_ABminus(EABminus); authData.setN_ABminus(NABminus);
+        authData.setE_Oplus(EOplus); authData.setN_Oplus(NOplus); authData.setE_Ominus(EOminus); authData.setN_Ominus(NOminus);
+        String response;
+        if(adding.checkForNoduplicateAuthorities(tax)==false){
+            response = adding.addAuthority(authData);
         }else{
             response = "invalid";
         }
@@ -159,32 +191,4 @@ public class registration {
     public String gExist_Ominus(){ return String.valueOf(authData.getE_Ominus()); }
     @GetMapping("/gNOm")
     public String gNeed_Ominus(){ return String.valueOf(authData.getN_Ominus()); }
-
-    @GetMapping("/gUn")
-    public String gUserName(){
-        return userData.getName();
-    }
-    @GetMapping("/gUi")
-    public String gUserId(){ return  String.valueOf(userData.getId()); }
-    @GetMapping("/gUa")
-    public String gUserAddress(){
-        return userData.getAddress();
-    }
-    @GetMapping("/gUr")
-    public String gUserRegion(){ return userData.getRegion(); }
-
-    @GetMapping("/gUc")
-    public String gUserCity(){
-        DB db = new DB();
-        return db.getCity(userData.getRegion()); }
-
-    @GetMapping("/gUw")
-    public String gUserWeight(){ return String.valueOf(userData.getWeight()); }
-    @GetMapping("/gUg")
-    public String gUserAge(){ return String.valueOf(userData.getAge()); }
-    @GetMapping("/gUt")
-    public String gUserType(){
-        return userData.getBloodtype();
-    }
-
 }
