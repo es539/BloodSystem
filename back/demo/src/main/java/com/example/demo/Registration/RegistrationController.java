3c00018ef8631f0aa9ca61b9e21a1e00fa0a352a
Esraa Aboshady <78180296+es539@users.noreply.github.com>
2022-12-28 15:58:26 +0200

    Update RegistrationController.java

--- back/demo/src/main/java/com/example/demo/Registration/RegistrationController.java
+++ back/demo/src/main/java/com/example/demo/Registration/RegistrationController.java
@@ -1,9 +1,31 @@
 package com.example.demo.Registration;
-
-
 import com.example.demo.DataBase.DB;
 
 public class RegistrationController {
+//    public Boolean checkID(int id){
+//
+//    return true;
+//    }
+//    public Boolean checkEmail(){
+//        return true;
+//    }
+//    public Boolean checkPassword(){
+//        return true;
+//    }
+//    public void createUserAcc(){
+//
+//    }
+//    public void createAuthrityAcc(){
+//    }
+
+    public boolean validateUserInfo( long id,  String password){
+        DB start = new DB();
+        Boolean V = start.validateUser(id,password);
+        if(V)
+            return true;
+        return false;
+    }
+
     public boolean validateAuthorityInfo( String tax,  String password){
         DB start = new DB();
         Boolean V = start.validateAuthority(tax, password);
@@ -13,11 +35,8 @@ public class RegistrationController {
         return false;
     }
 
-    public boolean validateUserInfo( long id,  String password){
-        DB start = new DB();
-        Boolean V = start.validateUser(id,password);
-        if(V)
-            return true;
-        return false;
+    public Boolean signIn(){
+        return true;
     }
+
 }
