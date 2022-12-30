80af95e6862fb0ab4f7c74c3524a8246adc9bf1f
Esraa Aboshady <78180296+es539@users.noreply.github.com>
2022-12-31 01:03:48 +0200

    Update Acception.java

--- back/demo/src/main/java/com/example/demo/BloodTransfution/Acception.java
+++ back/demo/src/main/java/com/example/demo/BloodTransfution/Acception.java
@@ -1,9 +1,11 @@
 package com.example.demo.BloodTransfution;
 
 import com.example.demo.DataBase.DB;
-import com.example.demo.Registration.Authority;
 import com.example.demo.Registration.Registration;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.*;
 @RestController
@@ -231,17 +233,63 @@ public class Acception {
         }
         return res;
     }
+   Authority chosenAuth = new Authority();
+    public String acceptionTicket (){
+        String res ="";
+        final String QUERY = "select * from authority where authName = \"" + chosenAuth.getName() + "\" and address = \""+chosenAuth.getAddress()+"\";";
 
-    Authority chosenAuth = new Authority();
-    @GetMapping("/ChooseAuthority")
+        // Open a connection
+        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+             Statement stmt = conn.createStatement();
+        ) {
+            String sql = "USE systemdb";
+            stmt.executeUpdate(sql);
+            ResultSet rs = stmt.executeQuery(QUERY);
+            while (rs.next()) {
+                //Display values
+                chosenAuth.setPhone(rs.getString("phone"));
+                chosenAuth.setCity(rs.getString("city"));
+                chosenAuth.setRegion( rs.getString("region"));
+                chosenAuth.setStartWork(rs.getString("workinghours_start"));
+                chosenAuth.setEndWork( rs.getString("workinghours_close"));
+                chosenAuth.setDonationtimeFrom( rs.getString("donationtimeFrom"));
+                chosenAuth.setDonationtimeTo(rs.getString("donationtimeTo"));
+            }
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
+        return "done";
+    }
+
+    @GetMapping("/chooseAuth")
     String chosenAuth(@RequestParam String name, @RequestParam String address){
         chosenAuth.setName(name);
         chosenAuth.setAddress(address);
         System.out.println("name" + chosenAuth.getName());
         System.out.println("address" + chosenAuth.getAddress());
-        return "Done";
-    }
+        return "Done";}
+    @GetMapping("/chosenAuthName")
+    String getChosenAuthName(){ return chosenAuth.getName(); }
+    @GetMapping("/chosenAuthAddress")
+    String getChosenAuthAddressName(){ return chosenAuth.getAddress(); }
 
+    @GetMapping("/chosenAuthPhone")
+    String getChosenAuthPhone(){ return chosenAuth.getPhone(); }
+    @GetMapping("/chosenAuthRegion")
+    String getChosenAuthAddressRegion(){ return chosenAuth.getRegion(); }
+    @GetMapping("/chosenAuthCity")
+    String getChosenAuthCity(){ return chosenAuth.getCity(); }
+    @GetMapping("/chosenAuthWStart")
+    String getChosenAuthAddressWStart(){ return chosenAuth.getStartWork(); }
+    @GetMapping("/chosenAuthWClose")
+    String getChosenAuthWclose(){ return chosenAuth.getEndWork(); }
+    @GetMapping("/chosenAuthDFrom")
+    String getChosenAuthAddressDFrom(){ return chosenAuth.getDonationtimeFrom(); }
+    @GetMapping("/chosenAuthDTo")
+    String getChosenAuthAddressDTo(){ return chosenAuth.getDonationtimeTo(); }
 
+    @GetMapping("/UserName")
+    String getUserName(){ return Registration.userData.getName(); }
 
 }
