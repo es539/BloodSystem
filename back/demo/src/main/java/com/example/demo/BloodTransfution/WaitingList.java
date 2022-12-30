a42ed5ab9a2f916b9a5e121d93ee49873cda17c4
Salma Saeed <101990421+SalmaSaeed29@users.noreply.github.com>
2022-12-31 01:40:07 +0200

    Update WaitingList.java

--- back/demo/src/main/java/com/example/demo/BloodTransfution/WaitingList.java
+++ back/demo/src/main/java/com/example/demo/BloodTransfution/WaitingList.java
@@ -1,11 +1,20 @@
 package com.example.demo.BloodTransfution;
 
+import com.example.demo.Registration.Registration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.sql.*;
 
+@RestController
+@CrossOrigin
+@RequestMapping("/savior")
 public class WaitingList {
     static final String DB_URL = "jdbc:mysql://localhost:3306";
     static final String USER = "root";
-    static final String PASS = "aboshady2001@Esraa";
+    static final String PASS = "2972001333";
 
     public String getWaitingListAccept(){
         String res ="";
@@ -34,6 +43,43 @@ public class WaitingList {
         return res;
 
     }
+    public String setWaitingListAccept(String name,long id, String BT){
+        final String QUERY = "insert into waitingListAccept values('"+name+"'," + id+",'"+BT+"');";
+        System.out.println(QUERY);
+        try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            Statement stmt = conn.createStatement();
+        ) {
+            String sql = "USE systemdb";
+            stmt.executeUpdate(sql);
+            stmt.executeUpdate(QUERY);
+            System.out.println("inserted successfully...");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return "valid";
+    }
+
+    @GetMapping("/setWaiting")
+    public String setWaitingListDonate(){
+        String name = Registration.userData.getName();
+        long id = Registration.userData.getId();
+        String BT = Registration.userData.getBloodtype();
+
+        final String QUERY = "insert into waitingListDonate values('"+name+"'," + id+",'"+BT+"');";
+        System.out.println(QUERY);
+        try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            Statement stmt = conn.createStatement();
+        ) {
+            String sql = "USE systemdb";
+            stmt.executeUpdate(sql);
+            stmt.executeUpdate(QUERY);
+            System.out.println("inserted successfully...");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return "Done";
+    }
+
 
     public String getWaitingListDonate(){
         String res ="";
@@ -62,4 +108,6 @@ public class WaitingList {
         return res;
 
     }
+
+
 }
